3f0624d3d5ef9fb7233f793bb9ea1941bdc62d2e
William Manco <william.manco@aquest.it>
2019-07-17 17:59:52 +0200

    Compressed texture

--- packages/core/src/textures/resources/BufferResource.js
+++ packages/core/src/textures/resources/BufferResource.js
@@ -39,6 +39,35 @@ export default class BufferResource extends Resource
     }
 
     /**
+     * Get list of compressed texture formats.
+     * @param {PIXI.Renderer} renderer Upload to the renderer
+     */
+    getCompressedTextureFormats(renderer)
+    {
+        const gl = renderer.gl;
+
+        if (this.compressedTextureFormats === null)
+        {
+            this.compressedTextureFormats = [];
+
+            if (gl.getExtension('WEBGL_compressed_texture_pvrtc')
+                     || gl.getExtension('WEBGL_compressed_texture_s3tc')
+                     || gl.getExtension('WEBGL_compressed_texture_etc1')
+                     || gl.getExtension('WEBGL_compressed_texture_astc'))
+            {
+                const formats = gl.getParameter(gl.COMPRESSED_TEXTURE_FORMATS);
+
+                for (let i = 0; i < formats.length; i++)
+                {
+                    this.compressedTextureFormats.push(formats[i]);
+                }
+            }
+        }
+
+        return this.compressedTextureFormats;
+    }
+
+    /**
      * Upload the texture to the GPU.
      * @param {PIXI.Renderer} renderer Upload to the renderer
      * @param {PIXI.BaseTexture} baseTexture Reference to parent texture
@@ -48,39 +77,74 @@ export default class BufferResource extends Resource
     upload(renderer, baseTexture, glTexture)
     {
         const gl = renderer.gl;
+        const internalFormat = baseTexture.format;
+        const isCompressed = internalFormat !== renderer.gl.RGBA
+        && internalFormat !== renderer.gl.RGB
+        && this.getCompressedTextureFormats(renderer).indexOf(internalFormat) > -1;
 
         gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, baseTexture.premultiplyAlpha);
 
         if (glTexture.width === baseTexture.width && glTexture.height === baseTexture.height)
         {
-            gl.texSubImage2D(
-                baseTexture.target,
-                0,
-                0,
-                0,
-                baseTexture.width,
-                baseTexture.height,
-                baseTexture.format,
-                baseTexture.type,
-                this.data
-            );
+            if (isCompressed)
+            {
+                gl.compressedTexSubImage2D(
+                    baseTexture.target,
+                    0,
+                    0,
+                    0,
+                    baseTexture.width,
+                    baseTexture.height,
+                    internalFormat,
+                    this.data
+                );
+            }
+            else
+            {
+                gl.texSubImage2D(
+                    baseTexture.target,
+                    0,
+                    0,
+                    0,
+                    baseTexture.width,
+                    baseTexture.height,
+                    internalFormat,
+                    baseTexture.type,
+                    this.data
+                );
+            }
         }
         else
         {
             glTexture.width = baseTexture.width;
             glTexture.height = baseTexture.height;
 
-            gl.texImage2D(
-                baseTexture.target,
-                0,
-                glTexture.internalFormat,
-                baseTexture.width,
-                baseTexture.height,
-                0,
-                baseTexture.format,
-                glTexture.type,
-                this.data
-            );
+            if (isCompressed)
+            {
+                gl.compressedTexImage2D(
+                    baseTexture.target,
+                    0,
+                    internalFormat,
+                    baseTexture.width,
+                    baseTexture.height,
+                    0,
+                    this.data
+                );
+            }
+            else
+            {
+                gl.texImage2D(
+                    baseTexture.target,
+                    0,
+                    glTexture.internalFormat,
+                    baseTexture.width,
+                    baseTexture.height,
+                    0,
+                    internalFormat,
+                    glTexture.type,
+                    this.data
+                );
+            }
         }
 
         return true;
